796862fa41cfe9839fa44de16a1679a98d2c2859
IDEALEMX <lnxp5acnt@gmail.com>
2025-04-24 11:48:58 -0600

    Added a simple gameloop structure

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
+#include <raylib.h>
+#include <iostream>
+using namespace std;
 
 int main () {
+
+    // init app
+    cout << "Starting the game...";
+    InitWindow (600, 400, "test");
+
+    // gameloop
+    while (!WindowShouldClose ()) {
+        BeginDrawing ();
+
+        ClearBackground (SKYBLUE);
+
+        EndDrawing ();
+    }
+
+    // close app
+    CloseWindow ();
+
     return 0;
 }
